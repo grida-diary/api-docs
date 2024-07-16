a6eb444d5d85a4bb9ecebd1cf7c5b02845203e99
wwan13 <wwan13@naver.com>
2024-07-16 14:46:44 +0900

    fix : 문법 수정

--- scripts/update_swagger.py
+++ scripts/update_swagger.py
@@ -39,7 +39,7 @@ def write_swagger_json(issue_body):
         json_data["info"]["description"] = "last updated : " + time.strftime('%Y-%m-%d %H:%M:%S')
         json_data["servers"] = meta_data["servers"]
 
-        if (meta["authorize"]["enable"] == True):
+        if meta["authorize"]["enable"]:
             json_data["components"]["securitySchemes"] = meta["authorize"]["securitySchemes"]
             json_data["security"] = meta["authorize"]["security"]
 
